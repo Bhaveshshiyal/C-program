ac95b349e87c3847e072b58d4fcc835c86c78e0b
Bhaveshshiyal <132765698+Bhaveshshiyal@users.noreply.github.com>
2024-03-21 09:42:11 +0530

    Add files via upload

---
+++ Tostring.java
@@ -0,0 +1,19 @@
+class Laptop {
+    string name;
+    int age;
+
+    public string toString() {
+        return name + "name" + "gae" + age;
+    }
+
+}
+
+public class ToStringg {
+    public static void main(String args[]) {
+        Laptop obj = new Laptop();
+        obj.name = "bhavesh";
+
+        System.err.println(obj);
+
+    }
+}
